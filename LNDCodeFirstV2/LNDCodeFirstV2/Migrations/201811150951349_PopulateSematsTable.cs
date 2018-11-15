6139cb3d30efb9d02b045b09a289fb08a4cb6b58
babak35102 <babak35102@gmail.com>
2018-11-15 13:24:23 +0330

    PopulateSematsTable: SQLCommandIntoMigration

---
+++ LNDCodeFirstV2/LNDCodeFirstV2/Migrations/201811150951349_PopulateSematsTable.cs
@@ -0,0 +1,25 @@
+﻿namespace LNDCodeFirstV2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateSematsTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('کارشناس')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('کاربر رایانه')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('معاون دایره')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('رئیس دایره')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('متصدی امور بانکی')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('اپراتور')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('پیش خدمت')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('معاون اداره')");
+            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('رئیس اداره')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
