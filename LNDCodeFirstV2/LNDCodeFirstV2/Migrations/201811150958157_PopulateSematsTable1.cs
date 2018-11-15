65119c969860a68af0a87d9501eaa07eccf7a408
babak35102 <babak35102@gmail.com>
2018-11-15 13:39:24 +0330

    End Of Semat works for now!

---
+++ LNDCodeFirstV2/LNDCodeFirstV2/Migrations/201811150958157_PopulateSematsTable1.cs
@@ -0,0 +1,17 @@
+﻿namespace LNDCodeFirstV2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateSematsTable1 : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"INSERT INTO Semats (Name) VALUES ('آبدارچی')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
