c2e62310bda121255e875f3bc51ed8ae998eb2ec
babak35102 <babak35102@gmail.com>
2018-11-15 13:26:57 +0330

    Update-Database

--- LNDCodeFirstV2/LNDCodeFirstV2/Migrations/201811150951349_PopulateSematsTable.cs
+++ LNDCodeFirstV2/LNDCodeFirstV2/Migrations/201811150951349_PopulateSematsTable.cs
@@ -7,15 +7,15 @@
     {
         public override void Up()
         {
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('کارشناس')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('کاربر رایانه')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('معاون دایره')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('رئیس دایره')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('متصدی امور بانکی')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('اپراتور')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('پیش خدمت')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('معاون اداره')");
-            Sql(@"INSERT INTO TABLE Semats (Name) VALUES ('رئیس اداره')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('کارشناس')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('کاربر رایانه')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('معاون دایره')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('رئیس دایره')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('متصدی امور بانکی')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('اپراتور')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('پیش خدمت')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('معاون اداره')");
+            Sql(@"INSERT INTO Semats (Name) VALUES ('رئیس اداره')");
         }
         
         public override void Down()
