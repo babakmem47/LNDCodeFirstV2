bf939d5c4daf48a43572757a57f3dece875bab89
babak35102 <babak35102@gmail.com>
2018-11-16 17:08:32 +0330

    Add class Section.cs + SectionConfiguration.cs

---
+++ LNDCodeFirstV2/LNDCodeFirstV2/EntityConfigurations/SectionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using LNDCodeFirstV2.Models;
+
+namespace LNDCodeFirstV2.EntityConfigurations
+{
+    public class SectionConfiguration : EntityTypeConfiguration<Section>
+    {
+        public SectionConfiguration()
+        {
+            Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            /////////// Section Relation ///////////////
+            
+        }
+    }
+}
---
+++ LNDCodeFirstV2/LNDCodeFirstV2/Models/Section.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LNDCodeFirstV2.Models
+{
+    public class Section
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Setad Setad { get; set; }
+
+        public int SetadId { get; set; }
+
+    }
+}
