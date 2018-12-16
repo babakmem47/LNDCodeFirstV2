9b0d8e3f814779988fa0d9e30d0c78671700d986
babak35102 <babak35102@gmail.com>
2018-12-16 10:02:46 +0330

    Add Class Person.cs

---
+++ LNDCodeFirstV2/LNDCodeFirstV2/Models/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LNDCodeFirstV2.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public Semat Semat { get; set; }
+
+        public int? SematId { get; set; }
+
+        public Company Company { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        public Section Section { get; set; }
+
+        public int? SectionId { get; set; }
+
+        public Branch Branch { get; set; }
+
+        public int? BranchId { get; set; }
+
+        public Bajje Bajje { get; set; }
+
+        public int? BajjeId { get; set; }
+
+
+    }
+}
